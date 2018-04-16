b6bf5b7b7c31d2162d6f56487461a7c0bf1c9ba5
Luciano Nooijen <lucianonooijen@gmail.com>
2018-04-16 23:22:24 +0200

    Added server script

--- TODO.md
+++ TODO.md
@@ -1,11 +1,8 @@
 # TO DO
 
-* Add Handlebars support
 * Ignore node_modules in esdoc config
-* Create server script
 * Add badges from Coding Standards
 * Create MJML rendering
-* Add support for TS(?)
 * Create REST API for sending emails
 * Create user interface for sending emails
 * Add CI/CD integration (CircleCI/Travis)
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
 const express = require('express');
+const expressValidator = require('express-validator');
+const hbs = require('hbs');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,8 +12,23 @@ const port = process.env.PORT || 3000;
 // Later on, write the access logs to a separate file and only console.log non 200 codes
 app.use(morgan('combined'));
 
+// View engine
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+
+// Register handlebars partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.send('Oops, this service does not support get requests (yet).');
 });
 
 app.listen(port, () => console(`Server listening on port ${port}`));
